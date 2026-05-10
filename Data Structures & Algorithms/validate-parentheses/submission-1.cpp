4f89e6aa6ce138264b40544898d2659e1069d331
azuu-rgb <azurodriguezflores27@gmail.com>
2026-05-10 10:30:26 -0600

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        if(s.size() % 2 != 0)
+            return false;
+
+        if(s[0] == ')' || s[0] == '}' || s[0] == ']')  
+            return false;
+
+         for(char c : s){
+            if(c == '(' || c == '[' || c == '{')
+                st.push(c);
+
+            if(c == ')' || c == ']' || c == '}'){
+                if(!st.empty()){ 
+                    if(c - st.top() == 1 || c - st.top() == 2){
+                        st.pop();
+                    }else{
+                        return false;
+                    } 
+                }/*else{
+                    st.push(c);
+                }*/
+            }
+        }
+        return st.empty() ? true :false;
+    }
+};
